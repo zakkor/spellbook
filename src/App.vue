9a739676b460365fb6b5726c3c59b94b9696bdb1
zakkor <edward.partenie@gmail.com>
2019-02-18 18:14:38 +0000

    Stylus, Pug, eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  #app
+    img(alt='Vue logo', src='./assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js App')
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
-};
+    HelloWorld,
+  },
+}
 </script>
+
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif

53757513234c881a580e1ddb8d11aec202f89d17
Edward d'Albon <edward.partenie@gmail.com>
2019-09-03 10:11:59 +0300

    push to gh

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,14 +2,12 @@
 nav.navbar(ref='navbar' role='navigation', aria-label='main navigation')
   .container.flex
     .controls
-      nuxt-link(to='/').title.f-7.uppercase.mr-10.ph-3 {{ title }}
+      nuxt-link(to='/').title.font-6.uppercase.mr-10.ph-3 {{ title }}
 
       svg.collapse.ml-auto(@click='toggleResponsive' viewBox='0 0 800 600')
         path#top(d='M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200')
         path#middle(d='M300,320 L540,320')
         path#bottom(d='M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190', transform='translate(480, 320) scale(1, -1) translate(-480, -318) ')
-
-      //- img.collapse.ml-auto(@click='toggleResponsive' src='https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png' width='32' height='32')
 
     .links
       ul
